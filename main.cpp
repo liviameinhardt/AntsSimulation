03ba66464bdd0f24d4dcd544399ef05e9fc5658b
luizfernando608 <ziulluz@gmail.com>
2022-04-11 22:57:59 -0300

    Adding colors

--- main.cpp
+++ main.cpp
@@ -34,8 +34,9 @@ struct space_unit
 
     }
 
-    bool has_ant = false;
+    int ant_number=0;
     bool has_pheromone = false;
+    bool has_food = false;
     // Pointer to the current ant
     ant* ant_here;
     // Pointer to current pheromone
@@ -45,16 +46,47 @@ struct space_unit
 
     // 
     void set_ant(){
-        has_ant = true;
+        ant_number++;
         visualization = "*";
     }
 
     void remove_ant(){
-        has_ant = false;
+        ant_number--;
         visualization = " ";
     }
 
+    void set_pheromone(){
+        has_pheromone = true;
+    }
+    void remove_pheromone(){
+        has_pheromone = false;
+    }
+
+    void set_food(){
+        has_food = true;
+    }
+    void remove_food(){
+        has_food = false;
+    }
+
+
+    void generate_visualization(){
+        if ((ant_number==0)&(has_pheromone))
+        {
+            visualization = "\033[1;35m•\033[0m";
+        }
+        else if ((ant_number>0))
+        {
+            visualization = "\033[1;36m*\033[0m";
+        }
+
+        if(has_food){
+            visualization = "\033[1;43m \033[0m";
+        }
+    }
+
     void show(){
+        generate_visualization();
         cout << visualization;
     }
 };
@@ -76,8 +108,10 @@ struct space
     // Vector to store the pheromone
     vector<pheromone> pheromones;
 
+    // Vector to store food coordinates
+    vector<food> foods;
 
-    space(int w_dimension, int h_dimension)
+    space(int h_dimension, int w_dimension)
     {
         width = w_dimension;
         heigth = h_dimension;
@@ -102,6 +136,19 @@ struct space
         map[i][j].remove_ant();
     }
 
+    void set_pheromone(int i, int j){
+        map[i][j].set_pheromone();
+    }
+    void remove_pheromone(int i, int j){
+        map[i][j].remove_pheromone();
+    }
+
+    void set_food(int i, int j){
+        map[i][j].set_food();
+    }
+    void remove_food(int i, int j){
+        map[i][j].remove_food();
+    }
     void show_map(){
         // Lines to clear the teminal
         int x=0, y=10;
@@ -140,18 +187,19 @@ struct space
 
 int main(int argc, char const *argv[])
 {
-    space map(40, 15); 
+    space map(15, 40); 
     int w_postion = 0;
     int h_postion = 0;
+    map.set_food(10, 35);
 
-    for (int i = 0; i < 30; i++)
+    for (int i = 0; i < 15; i++)
     {
         map.set_ant(h_postion,w_postion);
         map.show_map();
         
         // Sleep in miliseconds
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        map.set_pheromone(i,i);
         map.remove_ant(h_postion,w_postion);
 
         // Generating random number
